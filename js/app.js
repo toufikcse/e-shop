743b03422138bd39b77947ca1a38a2d7a37e3de9
toufikcse <toufikur.rahman.toufik@gmail.com>
2021-10-06 16:51:20 +0600

    remove ui done

--- js/app.js
+++ js/app.js
@@ -102,16 +102,107 @@ const showProducts = (products) => {
       const image = product.image;
       const div = document.createElement("div");
       div.classList.add("product");
-      console.log
       div.innerHTML = `
-        <div>
+        <button id="btn-card" onclick="addToCart(\'${product.name}\', \'${product.price}\', \'${image}\', \'${product.id}\')">
             <img class="product-image" src=${image}></img>
             <h3>${product.name}</h3>
             <h5>Price: ${product.price}</h5>
-        </div>
+        </button>
         `;
       document.getElementById("all-products").appendChild(div);
     }
 };
 
 loadProducts();
+
+let idArray = [];
+let cartArray = [];
+const addToCart = (name, price, image, id) => {
+    // console.log(idArray);
+    if(idArray.includes(id)) {
+        const textcount = document.getElementById(id).innerText;
+        let count = parseInt(textcount);
+        count = count + 1;
+        document.getElementById(id).innerText = count;
+    }
+    else {
+        idArray.push(id);
+        const div = document.createElement("div");
+        div.classList.add("product");
+        div.innerHTML = `
+            <div class="added-product">
+                <img id="img-cart" class="img-cart-quantity" src="https://agbd.s3.amazonaws.com/media/images/Macbook-Aidr--M1-d-1.jpg" alt="">
+                <span id="${id}">1</span>
+                <span>${name}</span>
+                <span>BDT: ${price}</span>
+                <span onclick="removeFromCart(\'${id}\')">
+                    <i id="remove-icon" class="fas fa-trash-alt"></i>
+                </span>
+                
+            </div>
+            <hr>
+        `;
+        document.getElementById('add-products').appendChild(div);
+        cartArray.push(id);
+        console.log('cartArray push id = ', id);
+    }
+    // console.log(idArray);
+    subTotal('subtotal', price);
+    tax('tax');
+    updateTotal();
+};
+
+const removeFromCart = id => {
+    // console.log('id = ', id);
+    // console.log('cart array before: ', cartArray);
+    console.log('cartArray Length ', cartArray.length);
+    console.log('idArray Length', idArray.length);
+    let key = 0;
+    for(let i=0; i<cartArray.length; i++) {
+        if(cartArray[i] == id){
+            key = i;
+        }
+    }
+    // console.log('key = ', key);
+    let list = document.getElementById('add-products');
+    // console.log(list.childNodes);
+    list.removeChild(list.childNodes[key+1]);
+    cartArray.splice(key, 1);
+    idArray.splice(key, 1);
+    // console.log(cartArray);
+}
+
+const getInputValue = (id) => {
+    const element = document.getElementById(id).innerText;
+    const converted = parseFloat(element);
+    return converted;
+};
+
+const subTotal = (id, value) => {
+    const convertedOldPrice = getInputValue(id);
+    const convertPrice = parseFloat(value);
+    // discount
+    const preDiscount = getInputValue('discount');
+    const discount = (convertPrice/100) * 10;
+    const totaldiscount = preDiscount + discount;
+
+    const afterDiscountPrice = (convertPrice/100) * 90;
+    const total = convertedOldPrice + afterDiscountPrice;
+    document.getElementById(id).innerText = /*Math.round(total);*/ total.toFixed(2);
+    document.getElementById('discount').innerText = totaldiscount.toFixed(2);
+};
+
+const tax = id => {
+    const oldTax = getInputValue(id);
+    const textSubTotal = getInputValue('subtotal');
+    const subTotal = parseFloat(textSubTotal);
+    const tax = (subTotal/100) * 5;
+    document.getElementById(id).innerText = /*Math.round(total);*/ tax.toFixed(2);
+};
+
+//grandTotal update function
+const updateTotal = () => {
+    const grandTotal = getInputValue("subtotal") + getInputValue("tax");
+    document.getElementById("total").innerText = grandTotal.toFixed(2);
+    document.getElementById("pay-btn").innerText = grandTotal.toFixed(2);
+};
